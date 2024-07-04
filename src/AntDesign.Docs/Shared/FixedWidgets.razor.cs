9157ec5f488916752e21b4cd0faf81ea91454c48
James Yeung <shunjiey@hotmail.com>
2024-07-05 00:37:46 +0800

    Add project files.

---
+++ src/AntDesign.Docs/Shared/FixedWidgets.razor.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Collections.Generic;
+using Microsoft.Extensions.Localization;
+
+namespace AntDesign.Docs.Shared
+{
+    public partial class FixedWidgets
+    {
+        [Inject] private IStringLocalizer Localizer { get; set; }
+        [Inject] private IJSRuntime JS { get; set; }
+
+        private ElementReference _linkRef;
+
+        private string _themeFileUrl;
+
+
+        private async Task ChangeTheme(string theme)
+        {
+            _themeFileUrl = $"_content/AntDesign.Docs/css/{theme}.css";
+
+            _ = JS.InvokeVoidAsync(JSInteropConstants.AddElementToBody, _linkRef);
+            _ = JS.InvokeVoidAsync(JSInteropConstants.SetDomAttribute, "html", new Dictionary<string, string>
+            {
+                ["data-theme"] = theme
+            });
+        }
+    }
+}
---
+++ src/Shared/DocsFile.cs
@@ -0,0 +1,11 @@
+﻿namespace AntDesign.Docs
+{
+    public class DocsFile
+    {
+        public float Order { get; set; }
+
+        public string Title { get; set; }
+
+        public string Html { get; set; }
+    }
+}
